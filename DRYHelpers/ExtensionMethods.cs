0f3ea47c2faee9f8f3162853f3aa52f311590c9d
Alex Fielder <alex.fielder@gmail.com>
2021-08-14 20:25:40 +0100

    Inserted some extension methods that should really be in this file

--- DRYHelpers/ExtensionMethods.cs
+++ DRYHelpers/ExtensionMethods.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Office.Interop.Excel;
+﻿using Inventor;
+using Microsoft.Office.Interop.Excel;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -56,6 +58,46 @@ namespace DRYHelpers
                 sum += (characters[i] - 'A' + 1);
             }
             return sum;  // in this example, sum would be "1" representing the column # where Customer Name resides 
+        }
+
+        /// <summary>
+        /// Extension method used to allow searching for multiple extensions.
+        /// converted from here: https://stackoverflow.com/questions/3527203/getfiles-with-multiple-extentions
+        /// </summary>
+        /// <param name="dir"></param>
+        /// <param name="extensions"></param>
+        /// <returns></returns>
+        public static IEnumerable<FileInfo> GetFilesByExtensions(this DirectoryInfo dir, params string[] extensions)
+        {
+            if (extensions == null)
+            {
+                throw new ArgumentNullException("extensions");
+            }
+            IEnumerable<FileInfo> files = dir.EnumerateFiles("*.*", SearchOption.AllDirectories);
+            //.Where(Function(s As FileInfo) s.FullName.EndsWith(My.Settings.TemplateSearchString001) OrElse s.FullName.EndsWith(My.Settings.TemplateSearchString002))
+            //Return files
+            return files.Where((FileInfo f) => extensions.Contains(f.Extension));
+        }
+
+        /// <summary>
+        /// Extension method intended to negate the dumb mechanic that Inventor currently uses when copying Sketchblocks whereby any
+        /// renamed parameters are copied and created anew.
+        /// </summary>
+        /// <param name="SketchBlockDefExtending">the object being extended.</param>
+        /// <param name="SketchBlockToCopy">The sketchblockdefinition being copied</param>
+        /// <param name="DestinationPartDocument">The destination part document.</param>
+        /// <returns></returns>
+        public static SketchBlockDefinition CopyTo(this SketchBlockDefinition SketchBlockDefExtending, SketchBlockDefinition SketchBlockToCopy, PartDocument DestinationPartDocument)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public static TResult IfNotNull<TInput, TResult>(this TInput o, Func<TInput, TResult> evaluator) where TInput : class where TResult : class
+        {
+            if (o == null)
+                return null;
+            return evaluator(o);
         }
     }
 
